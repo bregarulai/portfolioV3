6af1475a1cfcfa7d89a771cb6e4675ea45343322
bregarulai <bregarulai@gmail.com>
2024-08-09 19:00:34 -0400

    Implement assets folder, Bento grid first card

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import { FaHome } from "react-icons/fa";
+
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
---
+++ components/ui/bento-grid.tsx
@@ -0,0 +1,106 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  header?: React.ReactNode;
+  icon?: React.ReactNode;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
+        className
+      )}
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
+    >
+      <div className={cn("h-full", id === 6 && "flex justify-center")}>
+        <div className="w-full h-full absolute">
+          {img && (
+            <Image
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover object-center")}
+              fill
+            />
+          )}
+        </div>
+        <div
+          className={cn(
+            "absolute right-0 -bottom-5",
+            id === 5 && "w-full opacity-80"
+          )}
+        >
+          {spareImg && (
+            <Image
+              src={spareImg}
+              alt={spareImg}
+              className="object-cover object-center w-full h-full !relative"
+              fill
+            />
+          )}
+        </div>
+      </div>
+
+      {id === 6 && (
+        <BackgroundGradientAnimation>
+          <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
+        </BackgroundGradientAnimation>
+      )}
+
+      <div
+        className={cn(
+          titleClassName,
+          "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
+        )}
+      >
+        <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+          {description}
+        </div>
+        <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+          {title}
+        </div>
+      </div>
+    </div>
+  );
+};
