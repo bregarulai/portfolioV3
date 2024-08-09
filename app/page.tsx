ada3b6ac112fcde041a9e186c1cf3d3481d6412b
bregarulai <bregarulai@gmail.com>
2024-08-09 12:58:20 -0400

    Implement Hero component, initialized aceternity and shadcn UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hello Porfolio</h1>
+    <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto px-5 sm:px-10">
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
